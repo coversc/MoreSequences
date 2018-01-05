b121bef8d3fdff8c1ae8aa609b56ce1bf7ff0e54
coversc <coversc@rose-hulman.edu>
2018-01-05 10:45:08 -0500

    sequence code

--- src/m4_accumulating_sequences.py
+++ src/m4_accumulating_sequences.py
@@ -18,11 +18,11 @@ def main():
     run_test_make_less_simple_string()
 
     # ------------------------------------------------------------------
-    # TODO: 8. Uncomment the tests below before working TO DO 9.
+    # DONE: 8. Uncomment the tests below before working TO DO 9.
     #   They launch annoying rg.RoseWindows on each run that you don't want
     #   until you get to TO DO 9 and 10.
     # ------------------------------------------------------------------
-    #run_test_draw_shapes()
+    run_test_draw_shapes()
     run_test_rectangles_from_circles()
 
 
